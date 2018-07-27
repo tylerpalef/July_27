46c4c691baf12ec5c8583aaf287cbc7f7306d72b
“Tyler <tylerpalef@gmail.com>
2018-07-27 12:21:57 -0400

    finished first exercise of the day

---
+++ exercise_1.rb
@@ -0,0 +1,36 @@
+class Task
+  def initialize
+    @description = description
+    @due_date = due_date
+  end
+
+  def description
+    @description
+  end
+
+  def due_date
+    @due_date
+  end
+
+  def description=(input)
+    @description = input
+  end
+
+  def due_date=(input)
+    @due_date = input
+  end
+
+end
+
+water_plants = Task.new
+water_plants.description = "Water plants in backyard"
+
+feed_cat = Task.new
+feed_cat.due_date = "6 pm"
+
+go_to_bathroom = Task.new
+go_to_bathroom.due_date = "Right now".upcase
+
+p water_plants
+p "I have to feed my cat by #{feed_cat.due_date}"
+p "I have to go to the bathroom #{go_to_bathroom.due_date}"
